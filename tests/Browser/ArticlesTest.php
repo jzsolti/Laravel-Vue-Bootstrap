ce6e7275899c0934364e2b2a4a7ba41a0a2a33c1
Jónás Zsolt <jonaszsolt@gmail.com>
2021-08-17 14:30:56 +0200

    git test

--- tests/Browser/ArticlesTest.php
+++ tests/Browser/ArticlesTest.php
@@ -66,6 +66,7 @@ class ArticlesTest extends DuskTestCase
                 ->waitForText('Edit article');
 
             $titleValue = $browser->inputValue('title');
+            
 
             $browser->type('title', ' ') // need a single space instead of a blank string or clear method
                 ->press('form button[type="submit"]')
